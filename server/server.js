31da4cd91e6d7b236ff8180225126e5e93d5e3f0
VladimirJarabica <vladojarabica@gmail.com>
2017-11-18 16:05:37 +0100

    init server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from 'path';
+
+// Inicializácia servera
+const app = express();
+
+// Definícia ciest "routes"
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
+app.get('/bundle.js', (req, res) => {
+  res.sendFile(path.join(__dirname, '../bundle.js'));
+});
+
+// Spustenie servera
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log('app is listening on port', PORT);
+});
